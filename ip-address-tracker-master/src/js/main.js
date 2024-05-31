65101cc4fd8df0cc5871f1d860bbd3538f47af17
chibuzorEbede <chibuzorebede@gmail.com>
2024-05-31 00:20:51 -0500

    map's functional

--- ip-address-tracker-master/src/js/main.js
+++ ip-address-tracker-master/src/js/main.js
@@ -1,51 +1,82 @@
-//get a users ip on initial page load
-
-//ip address by the user
-
-// const getUserIP = ()=>{
-//   //return  the ip in the ip field
-// }
-
-const getApiUrl = (userIP = "") => {
-  //get the userinput ip or leave empty to get public address
-
-  const apiUrl = `https://geo.ipify.org/api/v2/country,city?apiKey=`;
-  const apiKey = `at_2TnyIcaRkIbLWo4GkcQV9pPyjMndR`;
-  const ipAdress = userIP;
-
-  const url = `${apiUrl}${apiKey}&ipAddress=${ipAdress}`;
-
-  return url;
-};
-
-//make a request to the api and save the response
-
-let url = getApiUrl();
-
-const getInitialIPDetails = async (url) => {
-  let data = {};
-
-  try {
-    const details = await fetch(url);
-    data = await details.json();
-  } catch (error) {
-    console.log(error);
-  }
-
-  return data;
-};
-
-const apiResponse = await getInitialIPDetails(url);
-console.log("api response:", apiResponse);
-
-const { ip, location, as, isp } = apiResponse;
-
-// build the map
-
-var map = L.map("map").setView([location.lat, location.lng], 13);
-
+//initialize map
+var map = L.map("map");
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution:
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
+var myIcon = L.icon({
+  iconUrl: "../src/images/icon-location.svg",
+  iconSize: [48, 63],
+  iconAnchor: [22, 94],
+  popupAnchor: [-3, -76],
+  shadowSize: [68, 95],
+  shadowAnchor: [22, 94],
+});
+
+//setup form
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let inputIP = document.getElementById("input_ip").value;
+  //do some checks here on inputIP
+  !inputIP ? `` : inputIP;
+  let filledUrl = getApiUrl(inputIP);
+  getInitialIPDetails(filledUrl);
+});
+
+//build api url depending on user input
+
+const getApiUrl = (userIP = "") => {
+  const apiUrl = `https://geo.ipify.org/api/v2/country,city?apiKey=`;
+  const apiKey = `at_2TnyIcaRkIbLWo4GkcQV9pPyjMndR`;
+  const ipAddress = userIP;
+
+  const url = `${apiUrl}${apiKey}&ipAddress=${ipAddress}`;
+
+  return url;
+};
+
+let url = getApiUrl();
+
+const getInitialIPDetails = (url) => {
+  let data = {};
+
+  try {
+    fetch(url)
+      .then((res) => res.json())
+      .then((responseData) => {
+        //initialize the details from the response
+        setIPDetails(responseData);
+      });
+  } catch (error) {
+    console.log(error);
+  }
+  return data;
+};
+
+getInitialIPDetails(url);
+
+const setIPDetails = (data) => {
+  // select elements from DOM
+  const ipElement = document.querySelector("#user_ip");
+  const userLocationElement = document.querySelector("#user_location");
+  const userTimeZoneElement = document.querySelector("#user_timezone");
+  const userIspElement = document.querySelector("#user_isp");
+
+  //data from server
+  const { ip, location, isp } = data;
+
+  //set the IPdetails
+  ipElement.textContent = ip;
+  userLocationElement.textContent = `${location.region}, ${location.city} ${location.postalCode}`;
+  userTimeZoneElement.textContent = `UTC${location.timezone}`;
+  userIspElement.textContent = isp;
+
+  // re-build the map
+  map.setView([location.lat, location.lng], 13);
+  L.marker([location.lat, location.lng], { icon: myIcon }).addTo(map);
+  //end map
+};
